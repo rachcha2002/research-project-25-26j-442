dd44b8872c02bbdab2339cd161b1270bf787eba1
kavinda0126 <127762257+kavinda0126@users.noreply.github.com>
2026-02-25 23:24:03 +0530

    fixed Conversational AI :-)

--- services/user-service/models/User.js
+++ services/user-service/models/User.js
@@ -61,10 +61,6 @@ const userSchema = new mongoose.Schema({
   timestamps: true
 });
 
-// Index for faster queries
-userSchema.index({ email: 1 });
-userSchema.index({ googleId: 1 });
-
 // Hash password before saving
 userSchema.pre('save', async function(next) {
   if (!this.isModified('password')) {
